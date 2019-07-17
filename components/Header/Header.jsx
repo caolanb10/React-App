0c0409fd08454acdc25944aa8018710b927a3bdb
Caolan <barryc10@tcd.ie>
2019-07-17 20:13:43 +0100

    initial commit

---
+++ components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import classes from './Header.css';
+
+const Header = ({}) => (
+  <AppBar />
+);
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+
+export default Header;
---
+++ components/Header/HeaderContainer.js
@@ -0,0 +1,9 @@
+import { compose, withProps, withStateHandlers } from 'recompose';
+import Header from './Header';
+
+const initialState = {};
+
+const handlers = {};
+
+export default compose(
+)(Header);
